db9c199cf6cd400e566317542429391e8671d6b1
Infgonza <infantegonzalo.mdz@gmail.com>
2023-11-10 19:25:03 -0300

    Segundo commit

--- src/programacionparcial/concesionaria/Principal.java
+++ src/programacionparcial/concesionaria/Principal.java
@@ -7,25 +7,25 @@ public class Principal {
         
         Concesionaria c= new Concesionaria ("Racing");
         
-        // Vemos la lista de vehiculos
+        // Vemos la lista de vehiculos.
         c.listVehiculos();
-        // Agregamos un auto 
+        // Agregamos un auto.
         c.agregarVehiculo("Toyota", "Corolla", "Blanco", "ABC123", 20000, "2020", "auto", 4, 0, 0);
-        // Agregamos una moto
+        // Agregamos una moto.
         c.agregarVehiculo("Honda", "Titan", "Roja", "DEF456", 5000, "2019", "moto", 0, 125, 0); 
-        // Agregamos una camioneta
+        // Agregamos una camioneta.
         c.agregarVehiculo("Ford", "Ranger", "Negra", "GHI789", 30000, "2018", "camioneta", 0, 0, 1000);
-        // Intentamos agregar un vehiculo con la misma patente
+        // Intentamos agregar un vehiculo con la misma patente.
         c.agregarVehiculo("Toyota", "Hilux", "Blanca", "GHI789", 35000, "2020", "camioneta", 0, 0, 1000);
         
-        // Vemos la lista de vehiculos pero ahora con vehiculos cargados
+        // Vemos la lista de vehiculos pero ahora con vehiculos cargados.
         c.listVehiculos();
         
-        // Eliminamos un vehiculo
+        // Eliminamos un vehiculo.
         c.eliminarVehiculo("DEF456");
         // Intentamos eliminar un vehiculo que no existe.
         c.eliminarVehiculo("ASQ123");
-        
+        // Vemos de nuevo la lista de vehiculos pero ahora con un vehiculo eliminado.
         c.listVehiculos();
     }
     
